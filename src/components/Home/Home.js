d2a9609e09c8bdcdaf4919606e4faf30450dcc2f
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-12-01 18:27:19 -0400

    Initial commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> David Verde</strong>
+                <strong className="main-name"> Dario Alessio</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
